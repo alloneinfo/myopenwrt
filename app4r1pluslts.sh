368cff201341d6542b39f74e170f2221245f9479
Kanny.Chak <kannychak@qq.com>
2022-04-06 15:51:54 +0800

    update

--- app4r1pluslts.sh
+++ app4r1pluslts.sh
@@ -10,25 +10,26 @@ git clone --depth 1 https://github.com/Lienol/openwrt-package
 mv -n openwrt-package/* ./ ; rm -Rf openwrt-package
 
 git clone --depth=1 https://github.com/fw876/helloworld  && mvdir helloworld
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
+
+# svn co https://github.com/immortalwrt/packages/trunk/net/smartdns
+git clone --depth=1 https://github.com/pymumu/luci-app-smartdns
+
+git clone --depth=1 https://github.com/openwrt/packages/tree/master/utils/syncthing
 git clone --depth 1 https://github.com/alloneinfo/luci-app-syncthing
-svn co https://github.com/alloneinfo/myopenwrt/trunk/myPackages/adguardhome
+
+svn co https://github.com/kenzok8/jell/trunk/adguardhome
 git clone --depth 1 https://github.com/alloneinfo/luci-app-adguardhome
 
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
 
 svn co https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns/trunk/tencentcloud_ddns luci-app-tencentddns
-svn co https://github.com/coolsnowwolf/lede/trunk/package/network/services/shellsync
-svn co https://github.com/x-wrt/packages/trunk/net/nft-qos
-svn co https://github.com/x-wrt/luci/trunk/applications/luci-app-nft-qos
 
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 svn co https://github.com/Ysurac/openmptcprouter-feeds/trunk/luci-app-iperf
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt aliyundrive && mvdir aliyundrive
 
 svn co https://github.com/immortalwrt/packages/trunk/net/amule
-svn co https://github.com/immortalwrt/packages/trunk/net/gost
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-amule
 
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
@@ -54,8 +55,6 @@ svn co https://github.com/messense/aliyundrive-fuse/trunk/openwrt && mvdir openw
 svn co https://github.com/kiddin9/openwrt-packages/trunk/baidupcs-web
 svn co https://github.com/kiddin9/openwrt-packages/trunk/libcryptopp
 svn co https://github.com/kiddin9/openwrt-packages/trunk/vsftpd-alt
-
-svn co https://github.com/alloneinfo/myopenwrt/trunk/myPackages/my-default-settings
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
