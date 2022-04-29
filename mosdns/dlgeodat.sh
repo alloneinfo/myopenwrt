60c834677551d540635007e64da6772ea2bd79e4
Kanny.Chak <kannychak@qq.com>
2022-04-29 19:01:00 +0800

    update

--- mosdns/dlgeodat.sh
+++ mosdns/dlgeodat.sh
@@ -1,2 +1,4 @@
+rm geoip.dat
+rm geosite.dat
 curl -LJO https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 curl -LJO https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
